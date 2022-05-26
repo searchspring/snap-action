e82ff1af45258f9c806ab1abe375e422e082fda0
Dennis Konieczek <konieczekdennis@gmail.com>
2022-05-26 12:44:58 -0400

    further debugging

--- scripts/metrics/lighthouse.js
+++ scripts/metrics/lighthouse.js
@@ -20,10 +20,8 @@ const METRICS_DIR = './metrics/data';
 
 async function prepare() {
 	try {
-        console.log("got here1")
 		await fsp.stat(METRICS_DIR);
 	} catch (err) {
-        console.log("got here2")
 		// make metrics directory
 	    await fsp.mkdir(METRICS_DIR, { recursive: true });
 	}
@@ -35,17 +33,12 @@ async function generateMetrics() {
 	} catch (err) {
 		throw 'no lighthouse data found!';
 	}
-    console.log("got here3")
 
     const now = new Date()
     console.log("process.argv", process.argv)
-    const argv = minimist(process.argv.slice(2));
+    const argv = minimist(process.argv.slice(2),  { '--': true });
     console.log("argv", argv)
     console.log("github.context", github.context)
-    const inputvariables = core.getInput('variables');
-    const inputsiteId = core.getInput('siteId');
-    console.log("inputvariables", inputvariables)
-    console.log("inputsiteId", inputsiteId)
 
 
 	const lighthouseContents = await fsp.readFile(LIGHTHOUSE_FILE, 'utf8');
@@ -64,7 +57,7 @@ async function generateMetrics() {
         performance: toPercentage(performance),
     };    
 
-    const { siteId, branch, repository_owner, repository, issue_number } = argv;
+    const { siteId, branch, repository } = argv;
     const reportHTMLFile = representativeRun.htmlPath.split('/').pop();
     const report = `https://snapui.searchspring.io/${siteId}/.lighthouse/${branch}/${reportHTMLFile}`;
     //              https://snapui.searchspring.io/undefined/.lighthouse/undefined/localhost-_lighthouse_html-2022_05_26_14_47_08.report.html",
@@ -72,10 +65,11 @@ async function generateMetrics() {
         timestamp: now,
         type: 'snap-lighthouse',
         data: {
+            siteId,
             branch,
-            repository_owner,
+            repository_owner: github.repository_owner,
             repository,
-            issue_number,
+            issue_number: github.issue_number,
             report,
             summary
         }
@@ -84,7 +78,6 @@ async function generateMetrics() {
     const filename = `SnapAction-${repository}-pull_request}${now.getFullYear()}_${now.getMonth() + 1}_${now.getDate()}_${now.getHours()}${now.getMinutes()}.json`;
     const contents = JSON.stringify(obj, null, '  ');
 
-    console.log("got here4")
     await fsp.writeFile(`${METRICS_DIR}/${filename}`, contents);
 
     console.log(`Generated lighthouse metrics file: ${filename}`);
