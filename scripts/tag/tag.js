ebdaf5a75beabd448dd538a2c04664ac9905c65b
Dennis Konieczek <konieczekdennis@gmail.com>
2022-08-09 15:08:16 -0400

    add cmp util, remove metrics for testing, finish tag.js

--- scripts/tag/tag.js
+++ scripts/tag/tag.js
@@ -1,21 +1,33 @@
 const exit = require('process').exit;
 const getCliArgs = require('../utils/getCliArgs');
+const cmp = require('../utils/cmp');
 
 (async function () {
     try {
         const args = getCliArgs(['tags']);
-        console.log("tag.js: ")
-        console.log(args.tags);
-        // exit(args.tags)
-        // if(args.tags !== 'string' && args.siteId_Type !== 'object') {
-        //     console.log("Verify script requires 'siteId_Type' parameter with values either 'string' (single site) or 'object' (multi site)")
-        //     exit(1);
-        // }
+
+        if(!args.tags) {
+            console.error("tags.js did not recieve any tags")
+            exit(1);
+        }
+
+        const tags = args.tags.split(' ');
+        tags.sort(cmp);
         
+        const latestTag = tags[tags.length - 1];
+        let [tag, suffix] = latestTag.split('-');
+        suffix = Number(suffix);
 
+        if(isNaN(suffix)) {
+            // creates first suffix version
+            console.log(`${tag}-1`)
+            exit(0);
+        }
 
-       
+        // increment suffix
+        console.log(`${tag}-${++suffix}`)
         exit(0);
+        
     } catch (err) {
         console.error('Error during authentication');
         console.error(err);
