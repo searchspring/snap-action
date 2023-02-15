aa48049f9736ca4460e342f5281f7d5f9e799b4a
Dennis Konieczek <konieczekdennis@gmail.com>
2023-02-15 10:57:13 -0500

    add escape.js script to escape commitMessage

---
+++ scripts/utils/escape.js
@@ -0,0 +1,19 @@
+const exit = require('process').exit;
+const getCliArgs = require('../utils/getCliArgs');
+
+(async function () {
+    try {
+        const args = getCliArgs(['text']);
+
+        if(!args.text) {
+            exit(0)
+        }
+
+        console.log(args.text.replaceAll(`\n`, ``).replaceAll(`'`, `\'`).replaceAll(`"`,` \"`));
+        exit(0);        
+    } catch (err) {
+        console.error(`Error during escape.js - could not escape text: ${args.text}`);
+        console.error(err);
+        exit(1);
+    }
+})();
